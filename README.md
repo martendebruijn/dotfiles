132b3358dfd596e411ad8863006eb6ed5dfa7023
martendebruijn <marten@debruijn.io>
2022-07-20 21:07:39 +0200

    ✨ feat(install.sh): quick and dirty install script

--- README.md
+++ README.md
@@ -6,9 +6,28 @@ My personal dotfiles
 
 - [dotfiles](#dotfiles)
   - [Table of Contents](#table-of-contents)
+  - [Install](#install)
   - [Contributing](#contributing)
   - [Authors](#authors)
   - [License](#license)
+
+## Install
+
+Make sure to clone into `$HOME/`
+
+```sh
+git clone git@github.com:martendebruijn/dotfiles.git
+cd dotfiles
+./install.sh
+```
+
+Enter environment variables in `.zshenv`.
+
+You may need to give the script execute permissions:
+
+```sh
+sudo chmod +x install.sh
+```
 
 ## Contributing
 
---
+++ install.sh
@@ -0,0 +1,36 @@
+for file in .*;
+do
+ if [[ $file == "." || $file == ".." || $file == ".DS_Store" || $file == ".git" ]]; then
+    continue
+  fi
+  echo 🔗Linking $file...
+  ln -s $HOME/dotfiles/$file ~/$file
+  echo "✅Link made for $file\r\n"
+done
+
+cd ./zsh
+for file in .*;
+do
+ if [[ $file == "." || $file == ".." || $file == ".DS_Store" || $file == ".zshenv.example" ]]; then
+    continue
+  fi
+  echo 🔗Linking $file...
+  ln -s $HOME/dotfiles/zsh/$file ~/$file
+  echo "✅Link made for $file\r\n"
+done
+
+cd ../git
+for file in .*;
+do
+ if [[ $file == "." || $file == ".." || $file == ".DS_Store" ]]; then
+    continue
+  fi
+  echo 🔗Linking $file...
+  ln -s $HOME/dotfiles/git/$file ~/$file
+  echo "✅Link made for $file\r\n"
+done
+
+cd ../
+echo 🤐Creating .zshenv...
+cp ./zsh/.zshenv.example ~/.zshenv
+echo ✅Made .zshenv\nPlease enter all the environment variables
