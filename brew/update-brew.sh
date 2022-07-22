01f58827816ef7036cf927aede1586931617d2ad
martendebruijn <marten@debruijn.io>
2022-07-22 21:45:51 +0200

    🐛 fix(update-brew.sh): add git add

--- brew/update-brew.sh
+++ brew/update-brew.sh
@@ -5,6 +5,7 @@ cd ~/dotfiles/brew/
 brew bundle dump --force
 git status -s Brewfile
 if git status -s Brewfile | grep -q 'M Brewfile'; then
+  git add Brewfile
   git commit -m "🧹 chore(Brewfile): update brewfile"
   git push
 fi
