8730c5405414d3e65c9ebac192a4193325a0d1b2
martendebruijn <marten@debruijn.io>
2022-07-22 19:11:40 +0200

    ✨ feat(make-repo.sh): added license

--- git/make-repo.sh
+++ git/make-repo.sh
@@ -1,14 +1,14 @@
 # Generate new repository
-# ./make-repo.sh <name> <readme_template>
+# ./make-repo.sh <name> <readme_template> <?license>
+# Dependencies: [license](https://www.npmjs.com/package/license)
 mkdir $1
 cd $1
 git init
 cp $2 README.md
 sed -i '' 's/PROJECT_NAME/$1/g' README.md
 touch .gitignore
-# !! TODO: Choose LICENSE
-git add README.md .gitignore
-# git add LICENSE
+license $3
+git add README.md .gitignore LICENSE
 git commit -m "🎉 initial commit"
 # !! TODO: Prompt if I made a repo on github and gitlab
 git remote add origin git@github.com:$USER/$1
