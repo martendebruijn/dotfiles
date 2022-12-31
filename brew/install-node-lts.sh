03aa76771bb8c730f5ae4a2faa1e4296c4019748
martendebruijn <marten@debruijn.io>
2022-12-31 03:28:51 +0100

    :recycle: (install-node-lts) put some repeating code in a fn

--- brew/install-node-lts.sh
+++ brew/install-node-lts.sh
@@ -9,34 +9,32 @@
 # @raycast.author Marten de Bruijn
 # @raycast.authorURL https://www.github.com/martendebruijn
 
-
-# echo "export PATH='/opt/homebrew/opt/$LAST_VERSION_NAME/bin:$PATH'" >> ~/.zshrc
-# becomes -> export PATH="/opt/homebrew/opt/node@$LTS_VERSION/bin:$PATH"
-# we want -> export PATH="/opt/homebrew/opt/node@18/bin:$PATH"
-
 LAST_VERSION_NAME=$(brew search /node@\[0-9\]\[0-9\]/ | tail -1)
-# LAST_VERSION=$("$LAST_VERSION_NAME" | sed 's/node@//') # -> command not found
 LAST_VERSION=$(brew search /node@\[0-9\]\[0-9\]/ | tail -1 | sed 's/node@//')
 
-# Both the if and else are almost identical -> so make it DRY
+function add-node {
+  readonly TO_INSTALL=${1:?TO_INSTALL must be specified.}
+  brew list | grep 'node' | while read -r VERSION ; do
+    echo "🍺uninstalling $VERSION"
+    brew uninstall -q --ignore-dependencies "$VERSION"
+    sed -i '' "s#export PATH='/opt/homebrew/$VERSION/bin:$PATH'##" ~/dotfiles/zsh/.zshrc # does this even work? because $PATH becomes resolved
+  done
+  echo "🍺installing ${TO_INSTALL}"
+  brew install -q node@"${TO_INSTALL}"
+  START="export PATH="
+  MID="/opt/homebrew/opt/node@$LTS_VERSION/bin:"
+  # shellcheck disable=SC2016
+  END='$PATH'
+  echo "${START}${MID}${END}" >> ~/.zshrc
+}
+
 if (("$LAST_VERSION" % 2)); then
   echo '🟢last node version is not a lts version'
   LTS_VERSION=$(brew search /node@\[0-9\]\[0-9\]/ | tail -2 | head -1 | sed 's/node@//')
   LTS_INFO=$(brew info --json node@"$LTS_VERSION")
   if [ "$(echo "$LTS_INFO" | jq -r '.[].installed[0].version')" == null ]; then
     echo '🟢last lts version is not installed'
-    brew list | grep 'node' | while read -r line ; do
-    # hier kan node instaan toch?
-       echo "🍺uninstalling version $line"
-       brew uninstall -q --ignore-dependencies "$line"
-       # remove path from .zshrc
-       sed -i '' "s#export PATH='/opt/homebrew/$line/bin:$PATH'##" ~/dotfiles/zsh/.zshrc
-    done
-    echo "🍺installing latest node lts version (node@$LTS_VERSION)"
-    brew install -q node@"$LTS_VERSION"
-    # add lts version to path
-    # shellcheck disable=SC2016
-    echo 'export PATH="/opt/homebrew/opt/node@$LTS_VERSION/bin:$PATH"' >> ~/.zshrc
+    add-node "$LTS_VERSION"
   else
     echo '🟢last lts version already installed'
     # check if "node" is installed
@@ -56,17 +54,7 @@ else
   LTS_INFO=$(brew info --json "$LAST_VERSION_NAME")
   if [ "$(echo "$LTS_INFO" | jq -r '.[].installed[0].version')" == null ]; then
     echo "🟢last lts version ($LAST_VERSION_NAME) is not installed"
-    brew list | grep 'node' | while read -r line ; do
-       echo "🍺uninstalling version $line"
-       brew uninstall -q --ignore-dependencies "$line"
-       # remove path from .zshrc
-       sed -i '' "s#export PATH='/opt/homebrew/$line/bin:$PATH'##" ~/dotfiles/zsh/.zshrc
-    done
-    echo '🍺installing latest node lts version'
-    brew install -q "$LAST_VERSION_NAME"
-    # add lts version to path
-      # shellcheck disable=SC2016
-    echo 'export PATH="/opt/homebrew/opt/$LAST_VERSION_NAME/bin:$PATH"' >> ~/.zshrc
+    add-node "$LAST_VERSION"
   else
     echo '🟢last lts version already installed'
   fi
