2f57562fcd2a943121363b1bc449b337f5ebf5f5
Marten de Bruijn <martendebruijn@outlook.com>
2024-11-18 11:20:07 +0100

    update gitignore

--- zsh/custom/personal-projects.zsh
+++
@@ -1,4 +0,0 @@
-alias mui="cd ~/Documents/personal/personal-coding-projects/ui"
-alias types="cd ~/Documents/personal/personal-coding-projects/types"
-alias utils="cd ~/Documents/personal/personal-coding-projects/utils"
-alias dot="cd ~/Documents/personal/personal-coding-projects/dotfiles"
